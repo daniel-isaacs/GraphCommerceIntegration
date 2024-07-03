cc46df96a9882b3cee1bb724e9fd366f43916095
Jonas Bergqvist <jonas.bergqvist@episerver.com>
2024-07-03 09:26:55 +0200

    .

--- graph-commerce-example-app/src/pages/[[...url]]/index.tsx
+++ graph-commerce-example-app/src/pages/[[...url]]/index.tsx
@@ -5,8 +5,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} data-epi-property-name="unstructuredData">
       <ProductListingComponent />
-    </main>
   );
 }
