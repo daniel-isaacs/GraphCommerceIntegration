fe1aa2a2a7f47f573d6d22790e748d15c557db22
Jonas Bergqvist <jonas.bergqvist@episerver.com>
2024-07-04 15:59:54 +0200

    .

--- graph-commerce-example-app/src/components/SearchTextComponent.tsx
+++ graph-commerce-example-app/src/components/SearchTextComponent.tsx
@@ -24,7 +24,7 @@ const SearchTextComponent: FC<SearchTextProps> = ({ searchText, setSearchText })
     };
 
     return (
-      <div className="relative flex items-center w-full h-full lg:w-64 group">
+      <div className="relative flex items-center w-full h-full lg:w-96 group">
           <input type="text" value={internalSearchText} onChange={handleSearchInput} onKeyDown={handleSearchboxKeyDown} className="block w-full py-1.5 pl-10 pr-4 leading-normal rounded-2xl focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 ring-opacity-90 bg-gray-100 dark:bg-gray-800 text-gray-400 aa-input" placeholder="Search"/>
           <button type="submit" onClick={handleSearchClick} className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-blue-500 focus:outline-none">
             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
